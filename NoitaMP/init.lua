33059f73447b63b427bfcfbffaf404ca4ae8711b
ttv/Ismoh_ <hertor3@mail.de>
2021-11-10 14:25:15 +0100

    init exmaple mod files

---
+++ NoitaMP/init.lua
@@ -0,0 +1,8 @@
+function OnPlayerSpawned( player_entity ) -- This runs when player entity has been created
+	GamePrint( "OnPlayerSpawned() - Player entity id: " .. tostring(player_entity) )
+end
+
+function OnWorldInitialized() -- This is called once the game world is initialized. Doesn't ensure any world chunks actually exist. Use OnPlayerSpawned to ensure the chunks around player have been loaded or created.
+	GamePrint( "OnWorldInitialized() " .. tostring(GameGetFrameNum()) )
+end
+
