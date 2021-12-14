fb0155640ab96b18f55ab6c2e2feb64aee6dc264
Ismoh_ <Ismoh@users.noreply.github.com>
2021-12-14 18:47:56 +0100

    Update file_util_spec.lua

--- spec/files/util/file_util_spec.lua
+++ spec/files/util/file_util_spec.lua
@@ -1,9 +1,17 @@
-describe("a test", function()
+require("file_util.lua")
+
+describe("file_util.lua", function()
   -- tests go here
 
-  describe("a nested block", function()
-    describe("can have many describes", function()
-      -- tests
+  describe("ReplacePathSeparator(path)", function()
+    it("is_windows = true", function()
+      _G.is_windows = true -- TODO: mock this on a better way: https://olivinelabs.com/busted/#spies-mocks-stubs
+      local path = "\test\path\123"
+      local result = ReplacePathSeparator(path)
+            
+            assert.has_no.errors(ReplacePathSeparator(path))
+            assert.are_not.equals(path, result)
+            assert.are.equals("//test//path//123", result)
     end)
   end)
 
