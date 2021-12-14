9e8e776dcd20d06d40fdd8955824f2f0c365254e
Ismoh_ <Ismoh@users.noreply.github.com>
2021-12-14 18:26:47 +0100

    Create file_util_spec.lua

---
+++ spec/files/util/file_util_spec.lua
@@ -0,0 +1,11 @@
+describe("a test", function()
+  -- tests go here
+
+  describe("a nested block", function()
+    describe("can have many describes", function()
+      -- tests
+    end)
+  end)
+
+  -- more tests pertaining to the top level
+end)
