6ca64abf63f775f4da166106af88dd30b52e4464
Ismoh_ <Ismoh@users.noreply.github.com>
2021-12-15 09:11:49 +0100

    Update file_util_spec.lua

--- spec/files/util/file_util_spec.lua
+++ spec/files/util/file_util_spec.lua
@@ -1,4 +1,4 @@
-require("noita-mp/files/scripts/util/file_util")
+local file_util = require("noita-mp/files/scripts/util/file_util")
 
 describe("file_util.lua", function()
   -- tests go here
@@ -7,9 +7,9 @@ describe("file_util.lua", function()
     it("is_windows = true", function()
       _G.is_windows = true -- TODO: mock this on a better way: https://olivinelabs.com/busted/#spies-mocks-stubs
       local path = "\test\path\123"
-      local result = ReplacePathSeparator(path)
+      local result = file_util.ReplacePathSeparator(path)
             
-            assert.has_no.errors(ReplacePathSeparator(path))
+            assert.has_no.errors(file_util.ReplacePathSeparator(path))
             assert.are_not.equals(path, result)
             assert.are.equals("//test//path//123", result)
     end)
@@ -17,9 +17,9 @@ describe("file_util.lua", function()
     it("is_windows = false (unix)", function()
       _G.is_windows = false -- TODO: mock this on a better way: https://olivinelabs.com/busted/#spies-mocks-stubs
       local path = "//test//path//123"
-      local result = ReplacePathSeparator(path)
+      local result = file_util.ReplacePathSeparator(path)
             
-            assert.has_no.errors(ReplacePathSeparator(path))
+            assert.has_no.errors(file_util.ReplacePathSeparator(path))
             assert.are_not.equals(path, result)
             assert.are.equals("\test\path\123", result)
     end)
