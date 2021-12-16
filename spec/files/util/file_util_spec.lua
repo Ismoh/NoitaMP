27a90c73bbbdb1231f8e3077a67d0231457468c3
ttv/Ismoh_ <hertor3@mail.de>
2021-12-16 09:34:02 +0100

    Install ffi with luarocks and changed cpath

--- spec/files/util/file_util_spec.lua
+++ spec/files/util/file_util_spec.lua
@@ -1,7 +1,8 @@
 package.cpath = package.cpath .. ";"
-.. string.gsub("${{ github.workspace }}/luajit/lib/?.dll;", "/", "\\")
-.. string.gsub("${{ github.workspace }}/luajit/lib/?.so;", "/", "\\")
+.. string.gsub("/home/runner/work/NoitaMP/NoitaMP/luajit/lib/?.dll;", "/", "\\")
+.. string.gsub("/home/runner/work/NoitaMP/NoitaMP/luajit/lib/?.so;", "/", "\\")
 
+print("package.cpath = " .. package.cpath)
 
 local fu = require("noita-mp/files/scripts/util/file_util")
 
