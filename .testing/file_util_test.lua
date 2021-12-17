9edc47b16b64cc903dd2b768f928e7f8bca65856
ttv/Ismoh_ <hertor3@mail.de>
2021-12-17 13:04:32 +0100

    Changed test 2

--- .testing/file_util_test.lua
+++ .testing/file_util_test.lua
@@ -4,18 +4,18 @@ local fu = require("noita-mp/files/scripts/util/file_util")
 
 function testReplacePathSeparatorWindows()
     _G.is_windows = true -- TODO: mock this on a better way: https://olivinelabs.com/busted/#spies-mocks-stubs
-    local path = "/test/path/123"
+    local path = "/test/path/123" -- unix path separator
     local result = fu.ReplacePathSeparator(path)
 
     print("path = " .. path)
     print("result = " .. result)
     lu.assertNotEquals(path, result)
-    lu.assertEquals("\\test\\path\\123", result)
+    lu.assertEquals([[\test\path\123]], result)
 end
 
 function testReplacePathSeparatorUnix()
     _G.is_windows = false -- TODO: mock this on a better way: https://olivinelabs.com/busted/#spies-mocks-stubs
-    local path = "\\test\\path\\123"
+    local path = [[\test\path\123]] -- windows path separator
     local result = fu.ReplacePathSeparator(path)
 
     print("path = " .. path)
