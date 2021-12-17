7c244c94de89d5b4b0ea19b1c8eaa4ade240e91c
ttv/Ismoh_ <hertor3@mail.de>
2021-12-17 13:19:04 +0100

    Test found the first issue!

--- .testing/file_util_test.lua
+++ .testing/file_util_test.lua
@@ -5,29 +5,31 @@ local fu = require("noita-mp/files/scripts/util/file_util")
 TestFileUtil = {}
 
 function TestFileUtil:setUp()
-    
+
 end
 
-function TestFileUtil:testReplacePathSeparatorWindows()
+function TestFileUtil:testReplacePathSeparatorOnWindows()
     _G.is_windows = true -- TODO: is there a better way to mock?
-    local path = "/test/path/123" -- unix path separator
-    local result = fu.ReplacePathSeparator(path)
 
-    print("path = " .. path)
-    print("result = " .. result)
-    lu.assertNotEquals(path, result)
-    lu.assertEquals([[\test\path\123]], result)
+    local path_unix = "/test/path/123"
+    local path_windows = fu.ReplacePathSeparator(path_unix)
+
+    print("path_unix = " .. path_unix)
+    print("path_windows = " .. path_windows)
+    lu.assertNotEquals(path_unix, path_windows)
+    lu.assertEquals([[\test\path\123]], path_windows)
 end
 
-function TestFileUtil:testReplacePathSeparatorUnix()
+function TestFileUtil:testReplacePathSeparatorOnUnix()
     _G.is_windows = false -- TODO: is there a better way to mock?
-    local path = [[\test\path\123]] -- windows path separator
-    local result = fu.ReplacePathSeparator(path)
 
-    print("path = " .. path)
-    print("result = " .. result)
-    lu.assertNotEquals(path, result)
-    lu.assertEquals("/test/path/123", result)
+    local path_windows = "\\test\\path\\123"
+    local path_unix = fu.ReplacePathSeparator(path_windows)
+
+    print("path_windows = " .. path_windows)
+    print("path_unix = " .. path_unix)
+    lu.assertNotEquals(path_windows, path_unix)
+    lu.assertEquals("/test/path/123", path_unix)
 end
 
 os.exit(lu.LuaUnit.run())
