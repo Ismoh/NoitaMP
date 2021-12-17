f5c6514246d81e4197895d0b1379cf01b3b66ba4
ttv/Ismoh_ <hertor3@mail.de>
2021-12-17 16:21:37 +0100

    Trying to create report with luacov

--- .github/workflows/adjust_package_paths.lua
+++
@@ -1,8 +0,0 @@
-print("adjust_package_paths.lua | Adjust luajits package.path to be able to run luarocks modules..")
-package.path = package.path .. ";/home/runner/work/NoitaMP/NoitaMP/luarocks/share/lua/5.1/?.lua"
-package.path = package.path .. ";/home/runner/work/NoitaMP/NoitaMP/luarocks/share/lua/5.1/?/init.lua"
-package.path = package.path .. ";/home/runner/work/NoitaMP/NoitaMP/luarocks/share/lua/5.1/?/?.lua"
-package.cpath = package.cpath .. ";/home/runner/work/NoitaMP/NoitaMP/luarocks/lib/lua/5.1/?.so"
-
-print("adjust_package_paths.lua | package.path = " .. package.path)
-print("adjust_package_paths.lua | package.cpath = " .. package.cpath)
