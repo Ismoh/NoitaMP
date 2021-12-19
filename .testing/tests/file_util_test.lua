318dfb56285868998d33029f7c412cb5162deaf7
ttv/Ismoh_ <hertor3@mail.de>
2021-12-19 16:39:52 +0100

    Tests are awesome! 10

--- .testing/tests/file_util_test.lua
+++ .testing/tests/file_util_test.lua
@@ -63,7 +63,7 @@ function TestFileUtil:testSetAbsolutePathOfNoitaRootDirectory()
 end
 
 function TestFileUtil:testGetRelativeDirectoryAndFilesOfSave06()
-    lu.assertError(fu.GetRelativeDirectoryAndFilesOfSave06(), true)
+    lu.assertErrorMsgEquals("Unix system are not supported yet :(", fu.GetRelativeDirectoryAndFilesOfSave06())
 end
 
 os.exit(lu.LuaUnit.run())
