0d2c35af3bd3a0912d9e04cb4c09cc5af307f4f8
ttv/Ismoh_ <hertor3@mail.de>
2021-12-24 00:42:31 +0100

    init_package_loading.lua was missing

--- noita-mp/init.lua
+++ noita-mp/init.lua
@@ -1,3 +1,6 @@
+-- Fix package.path and package.cpath once
+dofile_once("noita-mp/files/lib/external/init_package_loading.lua")
+
 dofile("mods/noita-mp/files/scripts/util/util.lua")
 local fu = require("file_util")
 
