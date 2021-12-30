e8f3bf23a1452bdb84f6992e6a36245ff1ffb0a8
ttv/Ismoh_ <hertor3@mail.de>
2021-12-30 13:00:51 +0100

    Fixing paths in 'dofile' 3

--- noita-mp/files/scripts/init/init_.lua
+++ noita-mp/files/scripts/init/init_.lua
@@ -1,4 +1,4 @@
 -- Noita independent init file for setting global object, requirements and fixing lua paths
-dofile("noita-mp/files/scripts/init/init_logger.lua")
 dofile("noita-mp/files/scripts/init/init_package_loading.lua")
+dofile("noita-mp/files/scripts/init/init_logger.lua")
 dofile("noita-mp/files/scripts/util/table_extensions.lua")
