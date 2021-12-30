727e31aafb1bda8c6202751df23821d9f66ff605
ttv/Ismoh_ <hertor3@mail.de>
2021-12-30 13:38:11 +0100

    Added file name to logger log 2

--- noita-mp/files/scripts/init/init_logger.lua
+++ noita-mp/files/scripts/init/init_logger.lua
@@ -1,9 +1,10 @@
 local Logging = require ("logging")
 
 local appender = function(self, level, message)
-  -- add file name to logs
+  -- add file name to logs: https://stackoverflow.com/a/48469960/3493998
   local file_name = debug.getinfo(2, "S").source:sub(2)
-  file_name = file_name:match("^.*/(.*).lua$") or file_name
+  file_name = file_name:match("^.*/(.*)$") or file_name
+  file_name = file_name .. ".lua"
 
   print(file_name, level, message)
   return true
