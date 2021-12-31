98df28385f99917f7a05972f91c5cf0f07293d61
Ismoh_ <Ismoh@users.noreply.github.com>
2021-12-31 04:01:37 +0100

    Just taking some notes

--- noita-mp/files/scripts/init/init_.lua
+++ noita-mp/files/scripts/init/init_.lua
@@ -2,3 +2,6 @@
 dofile("mods/noita-mp/files/scripts/init/init_package_loading.lua")
 dofile("mods/noita-mp/files/scripts/init/init_logger.lua")
 dofile("mods/noita-mp/files/scripts/util/table_extensions.lua")
+
+-- use require instead of dofile, because of different paths. Set package parh in the first line
+-- or move/copy noita-mp to ./mods/ on github
