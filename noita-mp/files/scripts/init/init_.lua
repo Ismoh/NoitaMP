6e3aeb0963c58cbea6e0a74212a7127e653caac9
ttv/Ismoh_ <hertor3@mail.de>
2022-01-21 17:49:33 +0100

    Trying to fix githubs workflow 5

--- noita-mp/files/scripts/init/init_.lua
+++ noita-mp/files/scripts/init/init_.lua
@@ -1,12 +1,17 @@
+-- Init lua scripts to set necessary defaults, like lua paths, logger init and extensions
+print("Initialise pathes, globals and extensions..")
+
 --#region
 -- github workflow stuff
 local params = ...
 print(("params = %s"):format(params))
 --#endregion
 
--- Init lua scripts to set necessary defaults, like lua paths, logger init and extensions
-print("Initialise pathes, globals and extensions..")
 dofile("mods/noita-mp/files/scripts/util/table_extensions.lua")
 dofile("mods/noita-mp/files/scripts/util/string_extensions.lua")
 dofile("mods/noita-mp/files/scripts/init/init_logger.lua")
-dofile("mods/noita-mp/files/scripts/init/init_package_loading.lua")(params)
+if params then
+    dofile("mods/noita-mp/files/scripts/init/init_package_loading.lua")(params)
+else
+    dofile("mods/noita-mp/files/scripts/init/init_package_loading.lua")
+end
