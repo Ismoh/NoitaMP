16fea424cfb58665b7ae431cbd872ac3ee170414
ttv/Ismoh_ <hertor3@mail.de>
2022-01-30 23:38:43 +0100

    Testing dofile params 8

--- noita-mp/files/scripts/init/init_.lua
+++ noita-mp/files/scripts/init/init_.lua
@@ -3,19 +3,21 @@ print("Initialise pathes, globals and extensions..")
 
 --#region
 -- github workflow stuff
+local destination_path = nil
 local varargs = {}
 -- https://stackoverflow.com/a/7630202/3493998
 for i = 1, select("#", ...) do
     varargs[#varargs + 1] = select(i, ...)
 end
-print(("varargs = %s"):format(unpack(varargs)))
+print("varargs = " .. unpack(varargs))
+destination_path = varargs[1]
 --#endregion
 
 dofile("mods/noita-mp/files/scripts/util/table_extensions.lua")
 dofile("mods/noita-mp/files/scripts/util/string_extensions.lua")
 dofile("mods/noita-mp/files/scripts/init/init_logger.lua")
--- if params then
-dofile("mods/noita-mp/files/scripts/init/init_package_loading.lua")(varargs)
--- else
+-- if destination_path then
+dofile("mods/noita-mp/files/scripts/init/init_package_loading.lua")(destination_path)
+--else
 --     dofile("mods/noita-mp/files/scripts/init/init_package_loading.lua")
--- end
+--end
