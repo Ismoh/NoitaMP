ad9d2b23636b3eea93e5f9797138dcae4d40a484
Ismoh <hertor3@mail.de>
2022-05-27 02:53:42 +0200

    Fixing unit tests #48

--- .testing/tests/mods/noita-mp/files/scripts/extensions/string_extensions_test.lua
+++ .testing/tests/mods/noita-mp/files/scripts/extensions/string_extensions_test.lua
@@ -1,7 +1,6 @@
 dofile("mods/noita-mp/files/scripts/init/init_.lua")
 
 local lu = require("luaunit")
-local stringExtensions = dofile("mods/noita-mp/files/scripts/extensions/string_extensions.lua")
 
 TestStringExtensions = {}
 
@@ -14,22 +13,22 @@ function TestStringExtensions:tearDown()
 end
 
 function TestStringExtensions:testExtendAndCutStringToLength()
-    lu.assertErrorMsgEquals("var is not a string.", stringExtensions.ExtendAndCutStringToLength, 1)
-    lu.assertErrorMsgEquals("length is not a number.", stringExtensions.ExtendAndCutStringToLength, "var", "length")
+    lu.assertErrorMsgEquals("var is not a string.", string.ExtendOrCutStringToLength, 1)
+    lu.assertErrorMsgEquals("length is not a number.", string.ExtendOrCutStringToLength, "var", "length")
     lu.assertErrorMsgContains(
         "char is not a character. string.len(char) > 1 = ",
-        stringExtensions.ExtendAndCutStringToLength,
+        string.ExtendOrCutStringToLength,
         "var",
         1,
         "character"
     )
 
     local expected = "12345"
-    local actual = stringExtensions.ExtendAndCutStringToLength("12345678910", 5, " ")
+    local actual = string.ExtendOrCutStringToLength("12345678910", 5, " ")
     lu.assertEquals(actual, expected)
 
     local expected2 = "1234      "
-    local actual2 = stringExtensions.ExtendAndCutStringToLength("1234", 10, " ")
+    local actual2 = string.ExtendOrCutStringToLength("1234", 10, " ")
     lu.assertEquals(actual2, expected2)
 end
 
