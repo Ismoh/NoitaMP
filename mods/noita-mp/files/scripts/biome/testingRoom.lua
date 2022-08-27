9e939b167d89decfb581344d2cee0148ff810aa2
Ismoh <hertor3@mail.de>
2022-08-27 21:44:35 +0200

    #48 Adding custom testing map.

---
+++ mods/noita-mp/files/scripts/biome/testingRoom.lua
@@ -0,0 +1,19 @@
+dofile_once("data/scripts/biome_scripts.lua")
+
+RegisterSpawnFunction(0xffffeedd, "init")
+RegisterSpawnFunction(0xffff0001, "spawn_enemy_a")
+RegisterSpawnFunction(0xffff0002, "spawn_enemy_b")
+
+function init(x, y)
+    LoadPixelScene("mods/noita-mp/data/biome_impl/biome_map.png", "", x, y, "", true)
+end
+
+function spawn_enemy_a(x, y)
+    EntityLoad("data/entities/animals/zombie_weak.xml", x, y)
+end
+
+function spawn_enemy_b(x, y)
+    EntityLoad("data/entities/animals/firemage_weak.xml", x, y)
+end
+
+function spawn_small_enemies() end
