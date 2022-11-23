3e4e1bb4c36070c7f412b80d9f807a49962ef4b3
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-11-23 13:19:53 +1100

    move cache file to be in the main folder and push dll

--- cache.c
+++ cache.c
@@ -1,18 +1,17 @@
-#include <lua.h>
-#include <lauxlib.h>
-// Test function that multiplies something by 50
+#include ".building/LuaJIT-2.0.4/src/lua.h"
+#include  ".building/LuaJIT-2.0.4/src/lauxlib.h"
 static int l_mult50(lua_State* L)
 {
     double number = luaL_checknumber(L, 1);
-    lua_pushnumber(L, number*50);
+    lua_pushnumber(L, number * 50);
     return 1;
 }
 
 int luaopen_cache(lua_State* L)
 {
-    static const luaL_reg cachelib [] =
+    static const luaL_reg cachelib[] =
     {
-         {"mult50", l_mult50}, 
+         {"mult50", l_mult50},
          {NULL, NULL}
     };
     luaL_register(L, "cache", cachelib);
