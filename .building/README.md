f51aaca641b18cd88f85f7a53e2b66a95ab3b69e
Ismoh <12631485+Ismoh@users.noreply.github.com>
2022-12-03 18:18:40 +0100

    #92 Looks like luarocks is working. package.paths aren't fully working yet.

--- .building/README.md
+++ .building/README.md
@@ -5,9 +5,10 @@
 3. Run the following command as *administrator* in `cmd.exe`:
    ```cmd
    REM // cd fullPathTo\NoitaMP\mods\noita-mp
+   set INIT_PATH=%CD%
    
    REM // resolve absolute path
-   set REL_PATH=..\.building\luarocks-3.9.1-windows-32
+   set REL_PATH=..\..\.building\luarocks-3.9.1-windows-32
    set ABS_PATH=
    rem // Save current directory and change to target directory
    pushd %REL_PATH%
@@ -19,6 +20,29 @@
    echo Maps to path: %ABS_PATH%
    
    REM // init luarocks
-   %ABS_PATH%\luarocks init -help
+   %ABS_PATH%\luarocks --lua-version=5.1 init --output=%INIT_PATH% --homepage=https://github.com/Ismoh/NoitaMP --lua-versions=5.1
    ```
    [source](https://stackoverflow.com/questions/1645843/resolve-absolute-path-from-relative-path-and-or-file-name)
+4. Result should look like this:
+   ```cmd
+   Initializing project 'noita-mp' for Lua 5.1 ...
+   -----------------------------------------------
+   
+   Checking your Lua installation ...
+   
+   Wrote template at D:\______BACKUP\NoitaMP_repo\NoitaMP\mods\noita-mp -- you should now edit and finish it.
+   
+   Adding entries to .gitignore ...
+   Preparing ./.luarocks/ ...
+   Wrote .luarocks/config-5.1.lua
+   Preparing ./lua_modules/ ...
+   Preparing ./luarocks.bat ...
+   Preparing ./lua.bat for version 5.1...
+   ```
+5. If you run `luarocks` in `\NoitaMP\mods\noita-mp` directory, it should work now.
+
+## Create a rockspec
+luarocks write_rockspec --license="GNU GPL v3" --lua-versions=5.1 --rockspec-format="3.0"
+
+## Run unit test
+luarocks test
---
+++ mods/noita-mp/tests/testAll.lua
@@ -0,0 +1,58 @@
+local lfs = require("lfs")
+
+function showFolder(folder)
+    print('=====================  ' .. folder)
+    local path = system.pathForFile(folder, system.ResourceDirectory)
+    for file in lfs.dir(path) do
+        print(file)
+    end
+end
+
+local isWindows = string.find(os.getenv("OS"), "indows")
+
+-- Lua implementation of PHP scandir function
+-- copy from file_util.lua
+function scanDir(directory)
+
+    if not directory or type(directory) ~= "string" then
+        error(("scanDir: directory is not a string or is nil: %s").format(directory))
+    end
+
+    local i, t, popen = 0, {}, io.popen
+    local pfile       = nil
+
+    if isWindows then
+        for dir in io.popen([[dir "]] .. directory .. [[" /b /ad]]):lines() do
+            i    = i + 1
+            t[i] = dir
+            print("dir: " .. dir)
+        end
+        pfile = popen('dir "' .. directory .. '" /b /ad')
+    else
+        pfile = popen('ls -a "' .. directory .. '"')
+    end
+
+    for filename in pfile:lines() do
+        i    = i + 1
+        t[i] = filename
+        print("filename: " .. filename)
+    end
+    pfile:close()
+    return t
+end
+
+local noitaMpDirectory = ""
+if isWindows then
+    noitaMpDirectory = io.popen("cd"):read()
+    print("currentDirectory: " .. noitaMpDirectory)
+else
+    error("Not implemented yet, because OS is not Windows. :(")
+end
+
+local testFiles = showFolder(lfs.currentdir() .. "/tests/")
+local testFiles = scanDir(noitaMpDirectory .. "/tests/")
+for i, file in ipairs(testFiles) do
+    if string.find(file, "test") then
+        dofile(noitaMpDirectory .. "/tests/" .. file)
+    end
+end
