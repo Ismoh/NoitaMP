4e4dea09058da7bce42c84da9b75bdbe6d782450
Ismoh <12631485+Ismoh@users.noreply.github.com>
2023-02-05 18:33:20 +0100

    #89 Removed zstd files and added version command.

--- mods/noita-mp/lua_modules/share/lua/5.1/zstd.lua
+++ mods/noita-mp/lua_modules/share/lua/5.1/zstd.lua
@@ -89,9 +89,10 @@ local ptr_zstd_inbuffer_t = ffi_typeof "ZSTD_inBuffer[1]"
 local ptr_zstd_outbuffer_t = ffi_typeof "ZSTD_outBuffer[1]"
 
 local fu = require("file_util")
-local path = fu.GetAbsolutePathOfNoitaRootDirectory() .. "/lua_modules/lib/lua/5.1/libzstd"
-print(path)
-local zstd = ffi_load(path)
+local zstd = ffi_load(fu.GetAbsolutePathOfNoitaRootDirectory() .. "/lua_modules/lib/lua/5.1/libzstd")
+local file    = assert(io.popen("zstd -vvV", "r"))
+local zstdVersion = file:read("*a")
+print("zstd version = " .. zstdVersion)
 
 
 local _M = {
