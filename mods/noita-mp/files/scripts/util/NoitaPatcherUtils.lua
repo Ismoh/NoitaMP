ecd50a1e13bfa4f77281f0b26517f681f720f21a
Ismoh <12631485+Ismoh@users.noreply.github.com>
2023-07-31 14:22:43 +0200

    Added md5 hash for storing in VSC instead of huge base64 string.

--- mods/noita-mp/files/scripts/util/NoitaPatcherUtils.lua
+++ mods/noita-mp/files/scripts/util/NoitaPatcherUtils.lua
@@ -2,6 +2,7 @@ local NoitaPatcherUtils = {}
 
 local np = require("noitapatcher")
 local base64 = require("base64")
+local md5 = require("md5")
 
 -- see https://dexter.xn--dpping-wxa.eu/NoitaPatcher/Example.html#example
 
@@ -9,20 +10,28 @@ local base64 = require("base64")
 -- time a projectile is fired. Functions are empty since
 -- we don't use these callbacks at the moment.
 function OnProjectileFired() end
+
 function OnProjectileFiredPost() end
 
----comment
----@param entityId number
----@return string encoded
+---Serialize an entity to a base64 and md5 string.
+---@param entityId any
+---@return string base64 
+---@return string md5
 function NoitaPatcherUtils.serializeEntity(entityId)
+    local cpc = CustomProfiler.start("NoitaPatcherUtils.serializeEntity")
     local binaryString = np.SerializeEntity(entityId)
     local encoded = base64.encode(binaryString)
-    return encoded
+    local hash = md5.sumhexa(encoded)
+    CustomProfiler.stop("NoitaPatcherUtils.serializeEntity", cpc)
+    return encoded, hash
 end
 
 function NoitaPatcherUtils.deserializeEntity(entityId, serializedEntityString, x, y)
+    local cpc = CustomProfiler.start("NoitaPatcherUtils.deserializeEntity")
     local decoded = base64.decode(serializedEntityString)
-    return np.DeserializeEntity(entityId, decoded, x, y)
+    local entityId = np.DeserializeEntity(entityId, decoded, x, y)
+    CustomProfiler.stop("NoitaPatcherUtils.deserializeEntity", cpc)
+    return entityId
 end
 
 return NoitaPatcherUtils
